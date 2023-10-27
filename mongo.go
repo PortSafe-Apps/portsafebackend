e54f884c8d1da54b7df202f7ea5769631bc1e2fb
fahiraaa06 <fahira1678@gmail.com>
2023-10-27 11:12:45 +0700

    add function

---
+++ mongo.go
@@ -0,0 +1,28 @@
+package peda
+
+import (
+	"os"
+
+	"github.com/aiteung/atdb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetConnection(MONGOCONNSTRINGENV, dbname string) *mongo.Database {
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: os.Getenv(MONGOCONNSTRINGENV),
+		DBName:   dbname,
+	}
+	return atdb.MongoConnect(DBmongoinfo)
+}
+
+func GCFGetHandle(mongoconn *mongo.Database, collection string) []User {
+	user := atdb.GetAllDoc[[]User](mongoconn, collection)
+	return user
+}
+
+func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User) bool {
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
+	return CheckPasswordHash(userdata.Password, res.Password)
+}
