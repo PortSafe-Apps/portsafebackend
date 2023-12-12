53da33f6775fa8481d574362cdbccb9c9806381d
fahiraaa06 <fahira1678@gmail.com>
2023-12-12 22:16:51 +0700

    upload to cloudflare

--- uploadimage.go
+++ uploadimage.go
@@ -4,94 +4,64 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"mime/multipart"
 	"net/http"
-	"path/filepath"
 )
 
 const (
-	r2BucketURL     = "https://c8cc7d3ddeb5397ee6f36830f52e4bc3.r2.cloudflarestorage.com/portsafeapps"
-	publicBucketURL = "https://pub-2ac92df5447e49a3aaf2415839c485a0.r2.dev/portsafeapps"
+	endpointURL = "https://c8cc7d3ddeb5397ee6f36830f52e4bc3.r2.cloudflarestorage.com"
+	bucketName  = "c8cc7d3ddeb5397ee6f36830f52e4bc3"
+	accessKeyId = "3fcc4c8aeb4b90d188d1250be36bf05d"
 )
 
-func UploadFile(r *http.Request) (string, error) {
+func UploadFile(w http.ResponseWriter, r *http.Request) {
 	// Parse the form file
-	imageFile, header, err := r.FormFile("image")
+	err := r.ParseMultipartForm(10 << 20) // 10 MB
 	if err != nil {
-		log.Printf("Error parsing form file: %v", err)
-		return "", err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
-	defer imageFile.Close()
 
-	// Create a new form file
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	formFilePart, err := writer.CreateFormFile("file", filepath.Clean(header.Filename))
+	file, handler, err := r.FormFile("image")
 	if err != nil {
-		log.Printf("Error creating form file part: %v", err)
-		return "", err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+	defer file.Close()
 
-	_, err = io.Copy(formFilePart, imageFile)
+	// Save the uploaded file to Cloudflare R2
+	err = SaveUploadedFileToR2(file, handler.Filename)
 	if err != nil {
-		log.Printf("Error copying file contents: %v", err)
-		return "", err
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	// Close the form data
-	writer.Close()
-
-	// Create a POST request to Cloudflare R2
-	url := fmt.Sprintf("%s/%s/photo.jpg", r2BucketURL, filepath.Clean(header.Filename))
-	req, err := http.NewRequest("POST", url, body)
-	if err != nil {
-		log.Printf("Error creating HTTP request: %v", err)
-		return "", err
-	}
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	// Perform the request
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Printf("Error performing HTTP request: %v", err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	// Check the response status
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("Error response body: %v", resp.Body)
-		return "", fmt.Errorf("cloudflare R2 upload failed with status code: %d", resp.StatusCode)
-	}
-
-	// Jika berhasil, dapatkan data setelah upload dari Cloudflare R2 menggunakan URL publik
-	uploadedData, err := getUploadedData(publicBucketURL, header.Filename)
-	if err != nil {
-		log.Printf("Error getting uploaded data: %v", err)
-		return "", err
-	}
-
-	// Lakukan sesuatu dengan uploadedData, misalnya, simpan ke basis data atau tampilkan informasi
-
-	return uploadedData, nil
+	// Send a response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("File uploaded successfully to Cloudflare R2"))
 }
 
-// Fungsi untuk mendapatkan data setelah upload dari Cloudflare R2 menggunakan URL publik
-func getUploadedData(publicURL, filename string) (string, error) {
-	url := fmt.Sprintf("%s/%s/photo.jpg", publicURL, filename)
-	resp, err := http.Get(url)
+func SaveUploadedFileToR2(file multipart.File, filename string) error {
+	// Membuat buffer untuk menyimpan file
+	fileBuffer := &bytes.Buffer{}
+	_, err := io.Copy(fileBuffer, file)
 	if err != nil {
-		return "", err
+		return err
+	}
+
+	// Menggunakan HTTP POST request untuk mengunggah file ke Cloudflare R2
+	url := fmt.Sprintf("%s/%s", endpointURL, filename)
+	resp, err := http.Post(url, "application/octet-stream", fileBuffer)
+	if err != nil {
+		return err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to upload file. Status: %s", resp.Status)
 	}
 
-	return string(body), nil
+	fmt.Println("File successfully uploaded to Cloudflare R2.")
+	return nil
 }
